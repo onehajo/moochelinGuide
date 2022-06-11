73719499b7555300ada6b22e6d75370a545182e4
juye <kjy_1615@naver.com>
2022-06-11 18:55:34 +0900

    로그아웃 만들었어ㅛㅇ

--- src/main/java/edu/kh/moochelinGuide/member/controller/LogoutServlet.java
+++ src/main/java/edu/kh/moochelinGuide/member/controller/LogoutServlet.java
@@ -10,36 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/member/profile/my")
-public class ProfileMyServlet extends HttpServlet{
-
+@WebServlet("/member/logout")
+public class LogoutServlet extends HttpServlet {
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-//		헤더에서 프로필클릭 시 요청받는 주소.
-		String path = "/WEB-INF/views/member/profile_my.jsp";
-		req.getRequestDispatcher(path).forward(req, resp);
-		
-		
-		// 로그인 세션 불러오기 
-		
 		HttpSession session = req.getSession();
 		
-		// 맵에다가 키밸류넣기
-		// 1) 평가한 영화 정보
-		// 2) 찜한 영화 정보
-		// 3) 취향분석 
+		session.invalidate();
 		
-		
-		
-		
-		
-		
-		
-		
-		
+    		
+		resp.sendRedirect( req.getContextPath() );
 		
 		
 	}
 	
+
 }
