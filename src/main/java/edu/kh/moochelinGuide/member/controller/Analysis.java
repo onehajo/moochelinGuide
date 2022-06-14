090ca15ca24c4ca2334444c8ebda8080834f828a
juye <kjy_1615@naver.com>
2022-06-14 22:41:38 +0900

    sql selectMemberUser 가 깃 문제로 코드가 바껴서 생긴 오류 해결 ㅠㅠ!

--- src/main/java/edu/kh/moochelinGuide/member/controller/Analysis.java
+++ src/main/java/edu/kh/moochelinGuide/member/controller/Analysis.java
@@ -1,6 +1,6 @@
-package analysis;
+package edu.kh.moochelinGuide.member.controller;
 
-import java.io.IOException;
+import java.io.IOException; 
 import java.util.Map;
 
 import javax.servlet.ServletException;
