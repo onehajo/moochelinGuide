0513322cb4d1e39c8485acf37814fcf55a279599
song_jh <thdwpgml11@naver.com>
2022-06-16 20:16:48 +0900

    푸터 평가한 영화 개수 조회 일부 수정

--- src/main/java/edu/kh/moochelinGuide/member/controller/EvaluationCountServlet.java
+++ src/main/java/edu/kh/moochelinGuide/member/controller/EvaluationCountServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import edu.kh.moochelinGuide.member.model.service.MemberService;
 
-@WebServlet("/member/evaltotal")
+@WebServlet("/evaltotal")
 public class EvaluationCountServlet extends HttpServlet{
 	
 	@Override
